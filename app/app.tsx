999f7064f86f87f0eb679021740663eccc593051
Ivan S Glazunov <ivansglazunov@gmail.com>
2025-05-07 21:51:41 +0300

    Update dependencies in package.json and package-lock.json for hasyx to version 0.1.102, and jwa to version 1.4.2. Modify build command in vercel.json for improved deployment process. Refactor app.tsx to enhance game components and integrate theme support. Introduce mounted hook for component lifecycle management. Update board component to accept custom background colors for improved UI flexibility. 🎨

--- app/app.tsx
+++ app/app.tsx
@@ -1,84 +1,159 @@
 "use client";
 
-import { signIn, signOut } from "next-auth/react";
-import { Github, LogIn, LogOut, MailCheck, MailWarning, Loader2 } from "lucide-react";
+import { LogOut, LoaderCircle, Crown, Joystick, X } from "lucide-react";
+import { signOut } from "next-auth/react";
 
-import { useClient, useSession, SessionData } from "hasyx";
+import { useClient, useSession } from "hasyx";
 import { Avatar, AvatarFallback, AvatarImage } from "hasyx/components/ui/avatar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "hasyx/components/ui/tabs";
 
-import { Card, CardContent } from "hasyx/components/ui/card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "hasyx/components/ui/carousel"
-import { useState } from "react";
-import { Button } from "hasyx/components/ui/button";
+import Board from "@/lib/board";
 import { OAuthButtons } from "hasyx/components/auth/oauth-buttons";
+import { useTheme } from "hasyx/components/theme-switcher";
+import { Button } from "hasyx/components/ui/button";
+import { Card } from "hasyx/components/ui/card";
+import { useState } from "react";
+import { Input } from "hasyx/components/ui/input";
+
+import { cn } from "hasyx/lib/utils"
+import { useMounted } from "@/hooks/mounted";
+
+export function GameCard({ disabled = false }: { disabled?: boolean }) {
+  return <Card className={`w-[min(15vw,15vh)] h-[min(30vw,30vh)] transform ${disabled ? "" : "hover:-translate-y-20 hover:scale-120"} transition-all duration-300 shadow-xl/30`}>
+  </Card>;
+}
+
+export function GameCards({ disabled = false, className }: { disabled?: boolean, className?: string }) {
+  return (
+    <div className={cn("absolute w-full flex items-center justify-center", className)}>
+      <div className="transform -rotate-35 translate-y-0"><GameCard disabled={disabled} /></div>
+      <div className="transform -rotate-20 translate-y-0"><GameCard disabled={disabled} /></div>
+      <div className="transform rotate-0 translate-y-0"><GameCard disabled={disabled} /></div>
+      <div className="transform rotate-20 translate-y-0"><GameCard disabled={disabled} /></div>
+      <div className="transform rotate-35 translate-y-0"><GameCard disabled={disabled} /></div>
+    </div>
+  );
+}
+
+export function GameClassic() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <>
+      <div className="aspect-square w-full h-full max-w-[min(70vw,70vh)] max-h-[min(70vw,70vh)]">
+        <Board position="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+          bgBlack={theme === "dark" ? '#3b0764' : '#c084fc'}
+          bgWhite={theme === "dark" ? '#581c87' : '#faf5ff'}
+        />
+      </div>
+    </>
+  );
+}
+
+export function GameFree() {
+  const session = useSession();
+  const mounted = useMounted();
+  const { theme, setTheme } = useTheme();
+  return (
+    <>
+      <div className="w-full flex flex-row relative z-2">
+        <div className="flex-grow"></div>
+        <div className={`bg-transparent border-none p-3`}>
+          <Avatar className={`relative transition-all duration-300 size-25 transform`}>
+            {!!session?.data?.user?.image && <AvatarImage src={session?.data?.user?.image} />}
+            <AvatarFallback>{session?.data?.user?.name || '?'}</AvatarFallback>
+            {session?.status === "loading" && <LoaderCircle className="animate-spin absolute top-0 left-0 w-full h-full"/>}
+          </Avatar>
+        </div>
+        <div className="flex-grow"></div>
+      </div>
+      <div className="aspect-square w-full h-full max-w-[min(70vw,70vh)] max-h-[min(70vw,70vh)]">
+        <Board position="qqqqkqqq/pppppppp/pppppppp/8/8/PPPPPPPP/PPPPPPPP/QQQQKQQQ w - - 0 1"
+          bgBlack={theme === "dark" ? '#3b0764' : '#c084fc'}
+          bgWhite={theme === "dark" ? '#581c87' : '#faf5ff'}
+        />
+      </div>
+      <GameCards className={`top-0 left-0 -rotate-180 transition-all duration-300 ${mounted ? "-translate-y-35" : "-translate-y-300"}`} disabled/>
+      <GameCards className={`bottom-0 left-0 transition-all duration-300 ${mounted ? "translate-y-40" : "translate-y-300"}`}/>
+    </>
+  );
+}
+
 export default function App() {
   const hasyx = useClient();
+  const { theme, setTheme } = useTheme();
   const session = useSession();
-  const [tab, setTab] = useState("user");
+  const [tab, setTab] = useState("classic");
+  const [profile, setProfile] = useState(false);
   
   const isAuthenticated = session?.status === "authenticated";
 
-  return <Tabs
-    defaultValue="user" className="flex flex-1 flex-col bg-purple-950 relative"
-    value={isAuthenticated ? tab : "auth"}
-    onValueChange={setTab}
-  >
-    <div className={`absolute top-0 left-0 w-full h-full transition-all duration-300 p-10 pb-25 flex items-center justify-center ${tab === "user"
-      ? `opacity-100 top-0`
-      : `opacity-0 top-full pointer-events-none`
-    }`}>
-      <Card className={`max-w-md h-full p-3 flex-1`}>
-        <Tabs defaultValue="account" className="w-full h-full flex flex-col">
-          <TabsList className="w-full">
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="payment">Payment</TabsTrigger>
-            <TabsTrigger value="notifications">Notifications</TabsTrigger>
-          </TabsList>
-          <div className="flex-1">
-            <TabsContent value="account" className="h-full flex flex-col">
-              <div className="flex-grow">
-              </div>
-              <div>
-                {isAuthenticated ? <Button variant="outline"
-                  className="w-full"
-                  onClick={() => signOut({ callbackUrl: '/' })} disabled={session.status === "loading"}
-                  >
-                  <LogOut className="mr-2 h-4 w-4" /> Sign Out
-                </Button>
-                : <OAuthButtons />}
-              </div>
-            </TabsContent>
-            <TabsContent value="payment">Payment Settings</TabsContent>
-            <TabsContent value="notifications">Notifications Settings</TabsContent>
+  return (<>
+    <Tabs
+      className="flex flex-1 flex-col bg-background relative"
+      value={isAuthenticated ? tab : "classic"}
+      onValueChange={setTab}
+    >
+      <div className="absolute top-3 right-3">
+        <Button variant="outline" size="icon" className="rounded-full">
+          <X/>
+        </Button>
+      </div>
+      <div className="flex-grow">
+        <TabsContent value="classic" className="flex items-center justify-center h-full">
+          <GameClassic />
+        </TabsContent>
+        <TabsContent value="free" className="flex flex-col items-center justify-center h-full relative">
+          <GameFree />
+        </TabsContent>
+      </div>
+
+      <div className={`absolute top-0 left-0 w-full h-full transition-all duration-300 p-10 pb-25 flex items-center justify-center ${profile
+        ? `opacity-100 top-0`
+        : `opacity-0 top-full pointer-events-none`
+      }`}>
+        <Card className={`max-w-md h-full p-3 flex-1 shadow-xl/30`}>
+          <Tabs defaultValue="account" className="w-full h-full flex flex-col">
+            <TabsList className="w-full">
+              <TabsTrigger value="account">Account</TabsTrigger>
+              <TabsTrigger value="payment">Payment</TabsTrigger>
+              <TabsTrigger value="notifications">Notifications</TabsTrigger>
+            </TabsList>
+            <div className="flex-1">
+              <TabsContent value="account" className="h-full flex flex-col">
+                <div className="flex-grow">
+                  <Input value={theme} onChange={(e) => setTheme(e.target.value)} />
+                </div>
+                <div>
+                  {isAuthenticated ? <Button variant="outline"
+                    className="w-full"
+                    onClick={() => signOut({ callbackUrl: '/' })} disabled={session.status === "loading"}
+                    >
+                    <LogOut className="mr-2 h-4 w-4" /> Sign Out
+                  </Button>
+                  : <OAuthButtons />}
+                </div>
+              </TabsContent>
+              <TabsContent value="payment">Payment Settings</TabsContent>
+              <TabsContent value="notifications">Notifications Settings</TabsContent>
+            </div>
+          </Tabs>
+        </Card>
+      </div>
+
+      <div className={`p-2 relative ${isAuthenticated ? "top-0" : "top-100vh"} transition-all duration-300`}>
+        <TabsList className="w-full h-15 bg-black bg-purple-900">
+          <TabsTrigger value="classic"><Crown/> Classic</TabsTrigger>
+          <div className={`bg-transparent border-none data-[state=active]:bg-transparent p-1 relative ${isAuthenticated ? "bottom-0" : "bottom-100vh"} transition-all duration-300`} onClick={() => setProfile(!profile)}>
+            <Avatar className={`relative transition-all duration-300 size-25 transform ${profile ? "scale-120 bottom-4" : "scale-100 bottom-2"
+              }`}>
+              {!!session?.data?.user?.image && <AvatarImage src={session?.data?.user?.image} />}
+              <AvatarFallback>{session?.data?.user?.name || '?'}</AvatarFallback>
+              {session?.status === "loading" && <LoaderCircle className="animate-spin absolute top-0 left-0 w-full h-full"/>}
+            </Avatar>
           </div>
-        </Tabs>
-      </Card>
-    </div>
-
-    <div className="flex-grow">
-      <TabsContent value="classic">Classic chess</TabsContent>
-      <TabsContent value="free">Free chess mode</TabsContent>
-    </div>
-
-    <div className={`p-2 relative ${isAuthenticated ? "top-0" : "top-full"}`}>
-      <TabsList className="w-full h-15 bg-black bg-purple-900">
-        <TabsTrigger value="classic">Classic</TabsTrigger>
-        <div className="bg-transparent border-none data-[state=active]:bg-transparent p-1" onClick={() => setTab(tab === "user" ? "classic" : "user")}>
-          <Avatar className={`relative transition-all duration-300 ${tab === "user" ? "size-25 bottom-8" : "size-20 bottom-3"
-            }`}>
-            {!!session?.data?.user?.image && <AvatarImage src={session?.data?.user?.image} />}
-            <AvatarFallback>{session?.data?.user?.name || '?'}</AvatarFallback>
-          </Avatar>
-        </div>
-        <TabsTrigger value="free">Free</TabsTrigger>
-      </TabsList>
-    </div>
-  </Tabs>
+          <TabsTrigger value="free"><Joystick/>Free</TabsTrigger>
+        </TabsList>
+      </div>
+    </Tabs>
+  </>);
 }
--- lib/board.tsx
+++ lib/board.tsx
@@ -8,6 +8,8 @@ interface BoardProps {
   position?: string;
   onMove?: (move: { from: string; to: string; promotion?: string }) => boolean;
   orientation?: 'white' | 'w' | 1 | 'black' | 'b' | 2;
+  bgBlack?: string;
+  bgWhite?: string;
 }
 
 /**
@@ -17,7 +19,9 @@ interface BoardProps {
 export default function Board({
   position = 'start',
   onMove,
-  orientation = 'white'
+  orientation = 'white',
+  bgBlack = '#cacaca',
+  bgWhite = '#fff'
 }: BoardProps) {
 
   // Handle piece drop (move)
@@ -39,7 +43,7 @@ export default function Board({
   const { width, height, ref } = useResizeDetector();
 
   return (
-    <div ref={ref} style={{ 
+    [<div key={bgBlack+bgWhite} ref={ref} style={{ 
       width: '100%',
       height: '100%',
       display: 'flex',
@@ -50,10 +54,10 @@ export default function Board({
         position={position}
         onPieceDrop={handlePieceDrop}
         boardWidth={width}
-        customDarkSquareStyle={{ backgroundColor: '#cacaca' }}
-        customLightSquareStyle={{ backgroundColor: '#fff' }}
+        customDarkSquareStyle={{ backgroundColor: bgBlack }}
+        customLightSquareStyle={{ backgroundColor: bgWhite }}
         boardOrientation={orientation === 'w' || orientation === 1 || orientation === 'white' ? 'white' : 'black'}
       />
-    </div>
+    </div>]
   );
 }
