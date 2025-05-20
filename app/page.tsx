b4af406c9c6a42519fba721641cf6c5296600fee
Ivan S Glazunov <ivansglazunov@gmail.com>
2025-05-20 21:43:40 +0300

    Update package version to 0.1.0-alpha.18, upgrade hasyx dependency to version 0.1.164, and enhance .npmignore by removing debug.ts from the published files. Refactor Telegram bot API route to streamline message handling and improve response structure. Update page.tsx to include package name in the breadcrumb for better navigation context.

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 
 import { SidebarLayout } from "hasyx/components/sidebar/layout";
 import sidebar from "@/app/sidebar";
-import App from "@/app/app";
+import pckg from "@/package.json";
+import App from "./app";
 
 // Now this is an async server component
 export default function Page() {
   return (
-    <SidebarLayout sidebarData={sidebar} title={'/'}>
+    <SidebarLayout sidebarData={sidebar} breadcrumb={[{ title: pckg.name, link: '/' }]}>
       <App />
     </SidebarLayout>
   );
